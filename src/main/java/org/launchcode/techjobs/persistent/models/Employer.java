452b1f49f60ed8ad925cabe0a590b91f558e14f6
showard1793 <Showard1793@gmail.com>
2024-11-10 14:57:33 -0600

    Updated - Finished Task #3.Add a Jobs Field to Employer

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,9 +16,16 @@ public class Employer extends AbstractEntity {
     @Size(max = 255, message = "Location must not be longer than 255 characters")
     private String location;
 
+    // Add a jobs field with @OneToMany and @JoinColumn
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
     // No-arg constructor
     public Employer() {
     }
+
+    //LOCATION
 
     // Getter
     public String getLocation() {
@@ -23,5 +35,18 @@ public class Employer extends AbstractEntity {
     // Setter
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    // JOBS
+    //Make Getter and Setter for jobs
+
+    // Getter
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    // Setter
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
