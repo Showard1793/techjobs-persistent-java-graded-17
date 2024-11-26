92f8844fa37c474d1bab5d48d003fc6e9f0e62db
showard1793 <Showard1793@gmail.com>
2024-11-26 17:33:41 -0600

    Updated - Finished

--- src/main/java/org/launchcode/techjobs/persistent/models/User.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/User.java
@@ -2,10 +2,36 @@ package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Objects;
+
 @Entity
-public class User extends AbstractEntity {
+public class User  {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User that = (User) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 
     @Column(unique = true) // Ensures database-level uniqueness
     private String username;
